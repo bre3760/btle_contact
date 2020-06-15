2e237bf580e5199040faaed43ae1f25382977e7c
bre3760 <57214701+bre3760@users.noreply.github.com>
2020-06-15 16:30:20 +0200

    Add files via upload

--- ble_scan_example.py
+++ ble_scan_example.py
@@ -1,39 +1,6 @@
 import time
 from bluepy.btle import Scanner, DefaultDelegate
 
-# class ScanDelegate(DefaultDelegate):
-#     def __init__(self):
-#         DefaultDelegate.__init__(self)
-
-#     def handleDiscovery(self, dev, isNewDev, isNewData):
-#         if isNewDev:
-#             print ("Discovered device", dev.addr)
-#         elif isNewData:
-#             print ("Received new data from", dev.addr)
-
-# scanner = Scanner().withDelegate(ScanDelegate())
-# devices = scanner.scan(5.0)
-
-# def getdistance(rssi):
-#     txpower = -59   #one meter away RSSI
-#     if rssi == 0:
-#         return -1
-#     else:
-#         ratio = rssi*1.0 / txpower
-#         if ratio < 1:
-#             return ratio ** 10
-#         else:
-#             return 0.89976 * ratio**7.7095 + 0.111
-
-# # for dev in devices:
-# #     print ("Device %s (%s), RSSI=%d dB, distance=%.2f meter" % (dev.addr, dev.addrType, dev.rssi, getdistance(dev.rssi)))
-
-# for dev in devices:
-#     print ("Device %s (%s), RSSI=%d dB, distance=%.2f meter" % (dev.addr, dev.addrType, dev.rssi, getdistance(dev.rssi)))
-#     for (adtype, desc, value) in dev.getScanData():
-#         print( " %s = %s" % (desc, value))
-#     print(" ")
-
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -59,22 +26,19 @@ def getdistance(rssi):
             return 0.89976 * ratio**7.7095 + 0.111
 
 
-def log_file(address):
-    logfile = open("log.txt","r+")
-    logaddress = logfile.readlines()
+def log(address, rssi, distance):
+    logfile = open("log.txt","a")
+    logfile.write(f"Device: {address} - RSSI: {rssi} - Distance: {distance} meter")
     logfile.close()
-    found = 0
-    for line in logaddress:
-        if address == line.rstrip():
-            found = 1
-    if found == 0:
-        logfile = open("log.txt","a")
-        logfile.write(f"{address}\n")
-        logfile.close()
 
 
 for dev in devices:
-    log_file(dev.addr)
-    print ("Device %s (%s), RSSI=%d dB, distance=%.2f meter" % (dev.addr, dev.addrType, dev.rssi, getdistance(dev.rssi)))
 
-    print(" ")
+    #erase the content of the previous log file
+    file = open("log.txt","r+")
+    file.truncate(0)
+    file.close()
+    distance = getdistance(dev.rssi)
+	log(dev.addr, dev.rssi, distance)
+    print ("Device %s (%s), RSSI=%d dB, distance=%.2f meter" % (dev.addr, dev.addrType, dev.rssi, distance))
+    print(" ")	
